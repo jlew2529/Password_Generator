de616b6d756dc6dae987ad9c3bd6c6dd4c680bdf
Jalen Lewis <jalenlew1995@gmail.com>
2020-10-28 01:00:58 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,26 @@ var generateBtn = document.querySelector("#generate");
 var passwordLength = Number(prompt("How long would you like your password be?"));
 
 while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 120) {
-  alert("Password length must be 8-100 characters.");
+  alert("You must input a number and the password length must be 8-100 characters.");
   passwordLength = Number(prompt("How long would you like you're password to be?"));
+}
+
+var lowerCase = confirm("Do you want to use lowercase letters?");
+
+while (!lowerCase) {
+  alert("You have to choose at least one type.");
+  lowerCase = confirm("Do you want to use lowercase letters?");
 }
 
 function generatePassword() {
   var pass = "";
-  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz";
+  var lLetters = "abcdefghijklmnopqrstuvwxyz";
 
-  for (var i = 1; i <= passwordLength; i++) {
-    var char = Math.floor(Math.random() * letters.length + 1);
-    pass += letters.charAt(char)
+  if (lowerCase === true) {
+    for (var i = 1; i <= passwordLength; i++) {
+      var char = Math.floor(Math.random() * lLetters.length + 1);
+      pass += lLetters.charAt(char)
+    }
   }
 
   return pass;
