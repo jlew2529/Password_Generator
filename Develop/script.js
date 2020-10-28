4641b2e40b6a4906cf2a44b25c7ea0498cfe06ea
Jalen Lewis <jalenlew1995@gmail.com>
2020-10-28 00:41:37 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordLength = Number(prompt("How long would you like your password be?"));
-var lowerCase = confirm("Do you want to use lowercase letters?");
-var upperCase = confirm("Do you want to use uppercase letters?");
-var numbers = confirm("Do you want to use numbers?");
-var characters = confirm("Do you want to use special characters?");
 
-
-
-while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 100) {
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 120) {
   alert("Password length must be 8-100 characters.");
   passwordLength = Number(prompt("How long would you like you're password to be?"));
 }
 
-while (!lowerCase && !upperCase && !numbers && !characters) {
-  alert("You have to choose at least one type.");
-  lowerCase = confirm("Do you want to use lowercase letters?");
-  upperCase = confirm("Do you want to use uppercase letters?");
-  numbers = confirm("Do you want to use numbers?");
-  characters = confirm("Do you want to use special characters?");
-  } 
+function generatePassword() {
+  var pass = "";
+  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz";
+
+  for (var i = 1; i <= passwordLength; i++) {
+    var char = Math.floor(Math.random() * letters.length + 1);
+    pass += letters.charAt(char)
+  }
+
+  return pass;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword ();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
 
-// Functions
-var randomFunction = {
-  lowercase: getLowerCase,
-  uppercase: getUpperCase,
-  number: getRandomNumber,
-  character: getRandomCharacter
-};
-
-function getLowerCase() {
-  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-
-function getUpperCase() {
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-
-function getRandomNumber() {
-  const numbers = "0123456789"
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function getRandomCharacter() {
-  const characters = "~!@#$%^&*()_+{}?/[]\/";
-  return characters[Math.floor(Math.random() * characters.length)];
 }
 
 // Add event listener to generate button
